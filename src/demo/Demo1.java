4dfef8f759eba3b056022d7e8eb326dea33481f3
PriyankaNikhade <109416967+PriyankaNikhade@users.noreply.github.com>
2022-07-16 20:03:48 +0530

    Update Demo1.java

--- src/demo/Demo1.java
+++ src/demo/Demo1.java
@@ -5,7 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hey buddy");
-
+System.out.println("123456");
 		System.out.println("Gm");
 	System.out.println("Bye");
 	System.out.println("Gn");
